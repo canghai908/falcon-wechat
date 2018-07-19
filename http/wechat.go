6e15139dac060ab605e11104695edf6b8ab5ac0a
canghai908 <cactifans@gmail.com>
2018-07-19 13:53:23 +0800

    fix

--- http/wechat.go
+++ http/wechat.go
@@ -22,15 +22,14 @@ func configProcRoutes() {
 		tos := param.MustString(r, "tos")
 		content := param.MustString(r, "content")
 		tos = strings.Replace(tos, ",", "|", -1)
-		var TokenServer = corp.NewDefaultAccessTokenServer(cfg.Wechat.CorpId, cfg.Wechat.Secret, nil)
 		body := new(send.Text)
 		body.ToUser = tos
 		body.MsgType = "text"
 		body.AgentId = cfg.Wechat.AgentId
 		body.Text.Content = content
-
-		clt := send.NewClient(TokenServer, nil)
-		if _, err := clt.SendText(body); err != nil {
+		TokenServer := corp.NewDefaultAccessTokenServer(cfg.Wechat.CorpId, cfg.Wechat.Secret, nil)
+		Clt := send.NewClient(TokenServer, nil)
+		if _, err := Clt.SendText(body); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			log.Println(err)
 		} else {
